b2613c7318dd1d73b24dbdb6a17abfec8a70adbf
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2023-04-29 12:39:29 -0300

    funcionando

--- KalderBlog/resources/views/site/home.blade.php
+++ KalderBlog/resources/views/site/home.blade.php
@@ -3,6 +3,36 @@
 @section('content')
 
 
-  <h1>TA FUNCIONANDO</h1>
+<div class="topo">
+        
+  <div class="container">
+      <div class="card">
+          <!-- conteúdo do card -->
+      </div>
+      <div class="card">
+          <!-- conteúdo do card -->
+      </div>
+      <div class="card">
+          <!-- conteúdo do card -->
+      </div>
+      <div class="card">
+          <!-- conteúdo do card -->
+      </div>
+  </div>
+
+    </div>
+    <footer>
+            <div class="right">
+                <h3>Pres. Prudente</h3>
+                <p>Rua Cidade Azul, 1945</p>
+                <p>Jardim Brasil</p>
+            </div>
+            <div class="left">
+                <h3>Contato</h3>
+                <p>+55 (18) 98805-8371</p>
+                <p>kalderblog@gmail.com</p>
+            </div>
+
+        </footer>
 
 @endsection  
--- KalderBlog/resources/views/site/layout.blade.php
+++ KalderBlog/resources/views/site/layout.blade.php
@@ -1,46 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home · AstroShows</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
-    <!-- Boot -->
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
-      integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
-      crossorigin="anonymous"
-    />
+    <!-- Css -->
+    <!-- @vite(['public/css/style.css'])
+    @vite(['public/css/custom.css']) -->
 
     <!-- Favicon -->
-    <link rel="icon" href="{{ Vite::asset('resources/images/outros/sound.png') }}"></link>
+    <link rel="icon" href="{{ Vite::asset('resources/images/outros/sound.png') }}">
+    </link>
     <!-- <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/146/146074.png"> -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
-  </head>
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Open+Sans:wght@300;400;500;700&display=swap"
+        rel="stylesheet">
+    <style>
+        * {
+            font-family: 'Bebas Neue', cursive;
+            color: white;
+            margin: 0;
+            padding: 0;
+
+        }
+
+        header {
+            position:fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            z-index: 9999;/* define a ordem de empilhamento, para que o cabeçalho fique na frente de outros elementos */
+            text-align: center;
+        }
+
+        a {
+            text-decoration: none;
+            color: white;
+            font-family: 'Open Sans', sans-serif;
+
+        }
+
+        img {
+            max-height: 20rem;
+        }
+
+        footer {
+            background-color: black;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            gap: 3rem;
+
+        }
+
+        footer p {
+            color: gray;
+            font-family: 'Open Sans', sans-serif;
+        }
+
+        .linha {
+            margin: 0 1rem 0 1rem
+        }
+
+        .container {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+            justify-items: center;
+            align-items: center;
+            height: 80%;
+            width: 80%;
+            margin: auto;
+            grid-column-gap: 0.5rem;
+        }
+
+        .rightalign {
+            text-align: right;
+        }
+
+        .card {
+            height: 20rem;
+            width: 20rem;
+            margin: 10px;
+            background-color: rgb(31, 30, 31);
+            border: linear-gradient(90deg, rgba(34, 27, 35, 1) 0%, rgba(27, 30, 29, 0.258140756302521) 65%, rgba(255, 255, 255, 1) 100%);
+        }
+
+        .right {
+            text-align: right;
+            width: 50%;
+        }
+
+        .left {
+            text-align: left;
+            width: 50%
+        }
+
+        /* .background-image {
+            background-image: url(foto.jpg);
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 100vh;
+        } */
+        .topo {
+          background-image: url(https://images.pexels.com/photos/7972038/pexels-photo-7972038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1);
+          background-repeat: no-repeat;
+          background-size: cover;
+        }
+    </style> 
+    </head>
 
   <body>
     <!-- NavBar -------------------------------------------------------->
-    <nav class="navbar navbar-expand-lg navbar-dark shadow-lg">
-   
-
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div>
-        <a class="btn btn-sm btn-outline-light" ">Perfil</a>
-      </div>
-    </nav>
+    <header > 
+            <h1>Kalder Blog</h1>
+            <div class="linha">
+                <a class="linha" href="">Home</a>
+                <a class="linha" href="">Sobre nós</a>
+                <a class="linha" href="">Perfil</a>
+            </div>
+        </header>
 
     <!-- Main -->
     <main role="main">
