c7789f024e24e1b79f534f6cee520a7ed2730094
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2023-04-29 13:41:46 -0300

    l

--- KalderBlog/resources/views/site/about.blade.php
+++ KalderBlog/resources/views/site/about.blade.php
@@ -3,22 +3,22 @@
 @section('content')
 
 <div class="topo">
- 
-<div class="mid">
-    <h1 class="left">Sobre nós</h1>
-    <p>Bem-vindo ao Kalker Blog, seu destino número um para descobrir as últimas notícias e tendências no mundo da
-    música! Criado em 24 de fevereiro de 2023, nosso blog é um espaço acolhedor e simples para aqueles que procuram
-    se manter atualizados sobre o que está acontecendo no cenário musical atual.
-    Embora sejamos um blog pequeno, nossa paixão pela música é grande. Nós nos dedicamos a trazer conteúdo de
-    qualidade para nossos leitores, mantendo nossas postagens rápidas e fáceis de ler. Nosso objetivo é fornecer uma
-    experiência de leitura agradável e informativa que os fãs de música vão adorar.
-    Nosso blog cobre uma ampla variedade de gêneros musicais, desde rock até pop, hip hop até música eletrônica e
-    tudo mais. Seja qual for o seu estilo de música favorito, estamos aqui para mantê-lo atualizado com as últimas
-    notícias, lançamentos e tendências.
-    Nosso compromisso é fornecer conteúdo atualizado regularmente, com postagens semanais que fornecem informações
-    valiosas e curiosidades sobre artistas e bandas. Além disso, também criamos listas de reprodução exclusivas e
-    curadorias de músicas que amamos e acreditamos que você também irá gostar.</p>
-</div>
+        <div class="">
+<div class="f">
+      <h1 class="left">Sobre nós</h1>
+      <p>Bem-vindo ao Kalker Blog, seu destino número um para descobrir as últimas notícias e tendências no mundo da
+        música! Criado em 24 de fevereiro de 2023, nosso blog é um espaço acolhedor e simples para aqueles que procuram
+        se manter atualizados sobre o que está acontecendo no cenário musical atual.
+        Embora sejamos um blog pequeno, nossa paixão pela música é grande. Nós nos dedicamos a trazer conteúdo de
+        qualidade para nossos leitores, mantendo nossas postagens rápidas e fáceis de ler. Nosso objetivo é fornecer uma
+        experiência de leitura agradável e informativa que os fãs de música vão adorar.
+        Nosso blog cobre uma ampla variedade de gêneros musicais, desde rock até pop, hip hop até música eletrônica e
+        tudo mais. Seja qual for o seu estilo de música favorito, estamos aqui para mantê-lo atualizado com as últimas
+        notícias, lançamentos e tendências.
+        Nosso compromisso é fornecer conteúdo atualizado regularmente, com postagens semanais que fornecem informações
+        valiosas e curiosidades sobre artistas e bandas. Além disso, também criamos listas de reprodução exclusivas e
+        curadorias de músicas que amamos e acreditamos que você também irá gostar.</p>
+    </div>
     </div>
     <footer>
             <div class="right">
