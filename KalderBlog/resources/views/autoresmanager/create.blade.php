9a3c88c67dc613e389d2c766d2c48c80fa360182
GabrielSilvaMEL <99516302+GabrielSilvaMEL@users.noreply.github.com>
2023-05-25 19:12:25 -0300

    autoresmanagercreate

--- KalderBlog/resources/views/autoresmanager/create.blade.php
+++ KalderBlog/resources/views/autoresmanager/create.blade.php
@@ -3,10 +3,27 @@
 @section('content')
 <div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 40%;">
     <center>
-        <h1>Criar post</h1>
+        <h1>Cadastrar Autor</h1>
     </center>
+    <div class="jumbotron">
+
+    <hr class="my-4">
+</div>
+<div class="container">
+     
+    @if ($errors->any())
+        <p></p>
+        <div class="alert alert-danger">
+            <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
         
-<!-- 70% Pronto. Faltando add imagem, e-mail e senha     -->
+<!-- 70% Pronto. Faltando add imagem, e-mail e senha -->
     <form style="margin: 0 auto;">
 
         <div class="width100">
@@ -30,7 +47,13 @@
             <p>Senha:</p>
             <input type="input" name="titulo" class="form-control" placeholder="" maxlength="50">
         </div>
-        
+        <hr class="my-4">
+
+        <!-- //? Cadastrar -->
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                    <button type="submit" class="btn btn-success">Cadastrar</button>
+                    <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
+            </div>
     </form>
     </div>
 
