c3592682d63e1d0e49b740c624dc7bcb710d8e7b
Guilherme HSBE <guilherm3hsb@gmail.com>
2023-06-10 16:39:59 -0300

    Revisão do AutoresManager - Index

--- KalderBlog/resources/views/autoresmanager/index.blade.php
+++ KalderBlog/resources/views/autoresmanager/index.blade.php
@@ -2,15 +2,14 @@
 
 @section('content')
 
-
 <div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 90%;">
     <center>
         <h1>Cadastro de Autores</h1>
     </center>
     <hr class="my-4">
-
-
     <a class="btn btn-success" href="{{ route('autoresmanager.create') }}">Cadastrar Artista</a>
+    <a class="btn btn-primary" href="{{ route('site.autores') }}"> Voltar</a>
+
     <p></p>
     @if ($message = Session::get('success'))
         <p></p>
@@ -18,44 +17,33 @@
             <p>{{ $message }}</p>
         </div>
     @endif
-        <table class="table table-bordered" style="color:white">
+        <table class="table table-dark table-striped table-hover" style="color:white">
         <tr>
             <th>#</th>
             <th>Nome</th>
-            <th>Imagem</th>
+            <th>Profissão</th>
+            <th>E-mail</th>
             <th width="280px">Ação</th>
         </tr>
         @foreach ($autores as $autor)
         <tr>
             <td>{{ ++$i }}</td>
             <td>{{ $autor->nome }}</td>
-            <td>{{ $autor->imagem }}</td>
+            <td>{{ $autor->descabreviada }}</td>
+            <td>{{ $autor->email }}</td>
             <td>
                 <form action="{{ route('autoresmanager.destroy', $autor->id) }}" method="POST">
-
                     <a class="btn btn-info" href="{{ route('autoresmanager.show', $autor->id) }}">Exibir</a>
-
                     <a class="btn btn-primary" href="{{ route('autoresmanager.edit', $autor->id) }}">Editar</a>
-
                     @csrf
                     @method('DELETE')
-
                     <button type="submit" class="btn btn-danger">Excluir</button>
                 </form>
             </td>
         </tr>
         @endforeach
     </table>
-
     {!! $autores->links() !!}
-
-    <div>
-        <center>
-
-            <a class="btn btn-success" href="{{ route('autoresmanager.create') }}">Cadastrar novo Autor</a>
-            <a class="btn btn-primary" href="{{ route('site.autores') }}"> Voltar</a>
-        </center>
-    </div>
 </div>
 
 @endsection
