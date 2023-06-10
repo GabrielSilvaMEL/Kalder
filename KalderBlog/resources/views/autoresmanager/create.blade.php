2b23a3e2ad5bf8bff18c0186e0bb41cb349ffa97
Guilherme HSBE <guilherm3hsb@gmail.com>
2023-06-10 17:16:22 -0300

    Revisão do AutoresManager - Create

--- KalderBlog/resources/views/autoresmanager/create.blade.php
+++ KalderBlog/resources/views/autoresmanager/create.blade.php
@@ -8,69 +8,68 @@
     
     <hr class="my-4">
 
-<div class="container">
-     
-    @if ($errors->any())
-        <p></p>
-        <div class="alert alert-danger">
-            <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    <div class="container">
         
-    <form action="{{ route('autoresmanager.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-        <div class="width100">
-        <strong>Nome:</strong>
-            <input type="input" name="nome" class="form-control" placeholder="Nome do autor" maxlength="50">
-        </div>
-        <br>
-        <div class="width100">
-        <strong>Profissão:</strong>
-            <input type="input" name="descabreviada" class="form-control" placeholder="Nome do autor" maxlength="50">
-        </div>
-        <br>
+        @if ($errors->any())
+            <p></p>
+            <div class="alert alert-danger">
+                <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+            
+        <form action="{{ route('autoresmanager.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+            <!-- Nome -->
+            <div class="width100">
+                <strong>Nome:</strong>
+                <input type="input" name="nome" class="form-control" placeholder="John Doe" maxlength="50">
+            </div>
+            <br>
 
-        <div class="width100">
-        <strong>Biografia:</strong>
-            <textarea type="texta" name="biografia" class="form-control" placeholder="Biografia" maxlength="2000"></textarea>
-        </div>
-        <br>
+            <!-- Profissão -->
+            <div class="width100">
+                <strong>Profissão:</strong>
+                <input type="input" name="descabreviada" class="form-control" placeholder="Músico e DJ" maxlength="50">
+            </div>
+            <br>
 
-        <!-- //* Imagem -->
-        <div class="col-xs-12 col-sm-12 col-md-12">
+            <!-- Biografia -->
+            <div class="width100">
+                <strong>Biografia:</strong>
+                <textarea type="texta" name="biografia" class="form-control" placeholder="Biografia breve do autor" maxlength="2000"></textarea>
+            </div>
+            <br>
+
+            <!-- Imagem -->
+            <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>Imagem:</strong>
                     <input type="file" name="imagem" class="form-control" placeholder="Imagem">
                 </div>
-        </div>
-        <br>
-
-        <div class="width100">
-        <strong>E-mail:</strong>
-            <input type="email" name="email" class="form-control" placeholder="nome@email.com" maxlength="50">
-        </div>
-        
-        <hr class="my-4">
-
-        <!-- //? Cadastrar -->
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
-                    <button type="submit" class="btn btn-success">Cadastrar</button>
             </div>
-    </form>
+            <br>
+
+            <!-- E-mail -->
+            <div class="width100">
+                <strong>E-mail:</strong>
+                <input type="email" name="email" class="form-control" placeholder="nome@email.com" maxlength="50">
+            </div>
+            
+            <hr class="my-4">
+
+            <!-- Botões -->
+            <!-- //? Cadastrar -->
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
+                <button type="submit" class="btn btn-success">Cadastrar</button>
+            </div>
+        </form>
     </div>
-
 </div>  
-</div>
-
-
-
-
-
 
 @endsection
