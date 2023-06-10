0fd9bdca444aa5415f624d6adfd7c7d3cdb9b53a
Guilherme HSBE <guilherm3hsb@gmail.com>
2023-06-10 14:36:09 -0300

    Revisão da Tabela Autor

--- KalderBlog/app/Models/Autor.php
+++ KalderBlog/app/Models/Autor.php
@@ -1,21 +1,22 @@
 <?php
 
+// Referências/Dependências
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Autor extends Model
 {
     use HasFactory;
+
+    // Correção de Tradução da Tabela "Autor"
     protected $table = "autores";
     
     protected $fillable = [
-        'nome',
-        'biografia',
-        'descabreviada',
-        'imagem',
-        'email',
+        'nome', // nome de exibição do autor
+        'biografia', // biografia breve
+        'descabreviada', // profissão/ocupação
+        'imagem', // imagem de exibição
+        'email', // email para contato
     ];
-    
 }
