f03b13038fe84f157a0529dbfc6c26086024241d
Guilherme HSBE <guilherm3hsb@gmail.com>
2023-06-10 17:33:32 -0300

    Revisão do AutoresManager - Show

--- KalderBlog/resources/views/autoresmanager/show.blade.php
+++ KalderBlog/resources/views/autoresmanager/show.blade.php
@@ -1,54 +1,62 @@
 @extends('site.layout')
 
 @section('content')
-<div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 90%;">
+
+<div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 70%;">
     <center>
-        <h1>Cadastro do autor {{ $autor->nome }}</h1>
+        <h1>Exibição de Cadastro do Autor {{ $autor->nome }}</h1>
     </center>
 
     <hr class="my-4">
+
+    <!-- Nome -->
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
-            <!-- //* Nome-->
-            <strong>Nome:</strong>
+            <strong>Nome</strong><br>
             {{ $autor->nome }}
         </div>
+    </div>
+    <br>
         
+    <!-- Biografia -->
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Biografia</strong><br>
+            {{ $autor->biografia }}
+        </div>
+    </div>
+    <br>
 
-        <!-- //* Descrição -->
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Biografia:</strong>
-                {{ $autor->biografia }}
-            </div>
+    <!-- Profissão -->
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Profissão</strong><br>
+            {{ $autor->descabreviada }}
         </div>
-        <!-- //* descabreviada -->
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Descrição abreviada:</strong>
-                {{ $autor->descabreviada }}
-            </div>
+    </div>
+    <br>
+
+    <!-- Imagem -->
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Imagem</strong><br>
+            {{ $autor->imagem }}
         </div>
-        <!-- //* Imagem -->
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Imagem:</strong>
-                {{ $autor->imagem }}
-            </div>
+    </div>
+    <br>
+
+    <!-- E-mail -->
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>E-mail</strong><br>
+            {{ $autor->email}}
         </div>
-        <!-- //* Email -->
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>E-mail:</strong>
-                {{ $autor->email}}
-            </div>
-        </div>
+    </div>
+    <br>    
         
-
-        <center>
-        <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
-        </center>
+    <center>
+    <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
+    </center>
 </div>
 
-</div>
 @endsection
