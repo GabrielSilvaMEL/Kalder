908a21b6611652c6f256b0bbcc39b75200c0585d
Guilherme HSBE <guilherm3hsb@gmail.com>
2023-06-10 17:27:31 -0300

    Revisão do AutoresManager - Edit

--- KalderBlog/resources/views/autoresmanager/edit.blade.php
+++ KalderBlog/resources/views/autoresmanager/edit.blade.php
@@ -2,76 +2,76 @@
 
 @section('content')
 
-<div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 90%;">
+<div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 40%;">
     <center>
-        <h1>Cadastro de Autores</h1>
+        <h1>Editar Cadastro de Autor</h1>
     </center>
+
     <hr class="my-4">
-<div class="container">
-    <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
-    <p></p>
-    @if ($errors->any())
+
+    <div class="container">
+        <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
         <p></p>
-        <div class="alert alert-danger">
-            <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
-    <form action="{{ route('autoresmanager.update', $autor) }}" method="POST">
-        @csrf
-        @method('PUT')
+        @if ($errors->any())
+            <p></p>
+            <div class="alert alert-danger">
+                <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-         <div class="row">
+        <form action="{{ route('autoresmanager.update', $autor) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-            <!-- //* Nome Artístico -->
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+            <div class="row">
+                <!-- Nome Artístico -->
+                <div class="width100 mb-4">
                     <strong>Nome:</strong>
-                    <input type="input" name="nome" value="{{ $autor->nome_artistico }}" class="form-control" placeholder="Nome" maxlength="50">
+                    <input type="text" name="nome" value="{{ $autor->nome }}" class="form-control" placeholder="John Doe" maxlength="50">
                 </div>
-            </div>
+                <br>
 
-            <!-- //* Descrição -->
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+                <!-- Descrição -->
+                <div class="width100 mb-4">
                     <strong>Biografia:</strong>
-                    <textarea class="form-control" style="height:150px" value="{{ $autor->biografia }}" name="biografia" placeholder="Descrição" maxlength="500">{{ $autor->biografia }}</textarea>
+                    <textarea class="form-control" style="height:150px" value="{{ $autor->biografia }}" name="biografia" placeholder="Músico e DJ" maxlength="500">{{ $autor->biografia }}</textarea>
                 </div>
-            </div>
+                <br>
 
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Descrição abreviada:</strong>
-                    <textarea class="form-control" value="{{ $autor->descabreviada }}" style="height:50px" name="descabreviada" placeholder="Descrição" maxlength="500">{{ $autor->biografia }}</textarea>
+                <!-- Profissão -->
+                <div class="width100 mb-4">
+                    <strong>Profissão:</strong>
+                    <input type="text" class="form-control" value="{{ $autor->descabreviada }}" name="descabreviada" placeholder="Biografia breve do autor" maxlength="500">
                 </div>
-            </div>
+                <br>
 
-            <!-- //* Imagem -->
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+                <!-- Imagem -->
+                <div class="width100 mb-4">
                     <strong>Imagem:</strong>
                     <input type="text" name="imagem" value="{{ $autor->imagem }}" class="form-control">
                 </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
+                <br>
+
+                <!-- E-mail -->
+                <div class="width100 mb-4">
                     <strong>E-mail:</strong>
-                    <input type="email" name="email" value="{{ $autor->email}}" class="form-control">
+                    <input type="email" name="email" value="{{ $autor->email}}" class="form-control" placeholder="johndoe@email.com">
+                </div>
+                <br>
+
+                <!-- Atualizar -->
+                <div class="width100 mb-4">
+                    <button type="submit" class="btn btn-success">Atualizar</button>
                 </div>
             </div>
-
-            <!-- //? Atualizar -->
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-              <button type="submit" class="btn btn-primary">Atualizar</button>
-            </div>
-        </div>
-
-    </form>
+        </form>
+    </div>
 </div>
 
 
