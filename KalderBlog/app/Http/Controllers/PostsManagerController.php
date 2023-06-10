ae52c55cc113c19f3cc5b763f1e890d21f8b4ab4
Guilherme HSBE <guilherm3hsb@gmail.com>
2023-06-10 15:49:48 -0300

    Revisão do PostsManagerController

--- KalderBlog/app/Http/Controllers/PostsManagerController.php
+++ KalderBlog/app/Http/Controllers/PostsManagerController.php
@@ -1,113 +1,103 @@
 <?php
 
+// Referências/Dependências
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Models\Autor;
 use App\Models\Post;
 
-
 class PostsManagerController extends Controller
 {
-    // * INDEX ---------------------------------|
+    // Index (página de exibição de posts cadastrados)
     public function index()
     {
-        $posts = Post::latest()->paginate(5);
-
-        return view('postsmanager.index',compact('posts'))
-                    ->with('i', (request()->input('page', 1) - 1) * 5);
+      $posts = Post::latest()->paginate(5);
+      return view('postsmanager.index',compact('posts'))
+                  ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
-    // * CREATE ---------------------------------|
+    // Create (inclusão de cadastro de posts)
     public function create()
     {
         $autores = Autor::all();
-        
         return view('postsmanager.create', compact('autores'));
     }
-    // * STORE ---------------------------------|
+
+    // Store (armazenamento das informações de cadastro)
     public function store(Request $request)
     {
-        $request->validate([
-            'titulo' => 'required',
-            'tituloresumido' => 'required',
-            'corpo' => 'required',
-            'datapub' => 'required',
-            'imagem',          
-            'autor_id' => 'required',
-            
-        ]);
+      $request->validate([
+          'titulo' => 'required',
+          'tituloresumido' => 'required',
+          'corpo' => 'required',
+          'datapub' => 'required',
+          'imagem',          
+          'autor_id' => 'required',
+      ]);
 
-        // Artista::create($request->all());
-
-        // Requisitar os campos no cadastro
+      # Requisitar os campos no cadastro
       $post = new Post;
-      $post->titulo = $request->titulo;
-      $post->tituloresumido = $request->tituloresumido;
-      $post->corpo = $request->corpo;
-      $post->datapub = $request->datapub;
-      $post->autor_id = $request->autor_id;    
+      $post->titulo = $request->titulo; #obrigatório
+      $post->tituloresumido = $request->tituloresumido; #obrigatório
+      $post->corpo = $request->corpo; #obrigatório
+      $post->datapub = $request->datapub; #obrigatório
+      $post->autor_id = $request->autor_id; #obrigatório
       $post->imagem = ""; #opcional
-        $dirImage = "images/posts";
+      $dirImage = "images/posts";
 
-        if($request->hasFile('imagem') && $request->file('imagem')->isValid()){
-            $requestImage = $request->imagem;
-            $extension = $requestImage->extension();
+      # Tratamento da imagem
+      if($request->hasFile('imagem') && $request->file('imagem')->isValid()){
+          $requestImage = $request->imagem;
+          $extension = $requestImage->extension();
 
-            // Hash para gerar nome da imagem com data e hora. (Não difere dos outros arquivos)
-            $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')) . "." . $extension;
-            
-            // Mover a imagem
-            $requestImage->move(public_path($dirImage), $imageName);
+          # Hash para gerar nome da imagem com data e hora. (Não difere dos outros arquivos)
+          $imageName = md5($requestImage->getClientOriginalName() . strtotime('now')) . "." . $extension;
+          
+          # Mover a imagem
+          $requestImage->move(public_path($dirImage), $imageName);
           $post->imagem = $dirImage . "/" . $imageName;
-        };
+      };
 
       $post->save();
-
-        return redirect()->route('postsmanager.index')->with('success','Post cadastrado com sucesso!');
+      return redirect()->route('postsmanager.index')->with('success','Post cadastrado com sucesso!');
     }
 
-    // * SHOW ---------------------------------|
+    // Show (exibição de cadastro individual do post)
     public function show($id)
     {
       $post = Post::findOrFail($id);
-
-        return view('postsmanager.show',compact('post'));
+      return view('postsmanager.show',compact('post'));
     }
 
-    // * EDIT ---------------------------------|
+    // Edit (edição das informações de cadastro de posts)
     public function edit($id)
     {
       $post = Post::findOrFail($id);
-        $autores = Autor::all();
-        
-        return view('postsmanager.edit',compact('post', 'autores'));
+      $autores = Autor::all();
+      return view('postsmanager.edit',compact('post', 'autores'));
     }
 
-    // * UPDATE ---------------------------------|
+    // Update (atualização recorrente da edição de cadastro de posts)
     public function update(Request $request, $id)
     {
-        $request->validate([
-            'titulo' => 'required',
-            'tituloresumido' => 'required',
-            'corpo' => 'required',
-            'datapub' => 'required',
-            'imagem' => 'required',          
-            'autor_id' => 'required',
-        ]);
+      $request->validate([
+          'titulo' => 'required',
+          'tituloresumido' => 'required',
+          'corpo' => 'required',
+          'datapub' => 'required',
+          'imagem' => 'required',          
+          'autor_id' => 'required',
+      ]);
 
-        $data = $request->all();
-        
-        Post::findOrFail($id)->update($data);
-
-        return redirect()->route('postsmanager.index')->with('success','Cadastro de post atualizado com sucesso!');
+      $data = $request->all();
+      Post::findOrFail($id)->update($data);
+      return redirect()->route('postsmanager.index')->with('success','Cadastro de post atualizado com sucesso!');
     }
 
-    // * DESTROY ---------------------------------|
+    // Destroy (exclusão de cadastro)
     public function destroy($id)
     {
-        Post::findOrFail($id)->delete();
-
-        return redirect()->route('postsmanager.index')->with('success','Cadastro de post excluído com sucesso!');
+      Post::findOrFail($id)->delete();
+      return redirect()->route('postsmanager.index')->with('success','Cadastro de post excluído com sucesso!');
     }
 }
