fc3de097d74a562e2ae6cd7b6ffc2cae1f242f84
Guilherme HSBE <guilherm3hsb@gmail.com>
2023-06-10 23:56:35 -0300

    Revisão do PostsManager - Create

--- KalderBlog/resources/views/postsmanager/create.blade.php
+++ KalderBlog/resources/views/postsmanager/create.blade.php
@@ -1,58 +1,64 @@
 @extends('site.layout')
 
 @section('content')
-<div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 90%;">
+
+<div class="mt-5 text-start mx-auto text-white p-3 div-abtus" style="width: 40%;">
     <center>
-        <h1>Cadastrar Post</h1>
+        <h1>Cadastrar Postagem</h1>
     </center>
     
     <hr class="my-4">
 
-<div class="container">
-     
-    @if ($errors->any())
-        <p></p>
-        <div class="alert alert-danger">
-            <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    <div class="container">
         
-    <form action="{{ route('postsmanager.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-        <div class="width100">
-        <strong>Título:</strong>
-            <input type="input" name="titulo" class="form-control" placeholder="Título do post" maxlength="300">
-        </div>
-        <br>
-
-        <div class="width100">
-        <strong>Titulo Resumido:</strong>
-            <input type="input" name="tituloresumido" class="form-control" placeholder="Titulo resumido" maxlength="200">
-        </div>
-        <br>
-
-        <div class="width100">
-        <strong>Corpo:</strong>
-            <textarea type="input" name="corpo" class="form-control" placeholder="Corpo do post" maxlength="10000"></textarea>
-        </div>
-        <br>
-        
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Imagem:</strong>
-                <input type="file" name="imagem" class="form-control" placeholder="Imagem">
+        @if ($errors->any())
+            <p></p>
+            <div class="alert alert-danger">
+                <strong>Ops!</strong> Houve algum problema com a entrada de dados.<br><br>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-        </div>
-        <br>
-        <!-- //* Autor -->
-        <div class="col-xs-12 col-sm-12 col-md-12">
+        @endif
+            
+        <form action="{{ route('postsmanager.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+            <!-- Título da Postagem -->
+            <div class="width100">
+                <strong>Título da Postagem</strong>
+                <input type="input" name="titulo" class="form-control" maxlength="500">
+            </div>
+            <br>
+
+            <!-- Título Resumido da Postagem -->
+            <div class="width100">
+                <strong>Titulo Resumido</strong>
+                <input type="input" name="tituloresumido" class="form-control" maxlength="250">
+            </div>
+            <br>
+
+            <!-- Corpo -->
+            <div class="width100">
+                <strong>Corpo da Postagem</strong>
+                <textarea type="input" name="corpo" class="form-control" maxlength="10000"></textarea>
+            </div>
+            <br>
+            
+            <!-- Imagem -->
+            <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
-                    <strong>Autor: </strong>
+                    <strong>Imagem</strong>
+                    <input type="file" name="imagem" class="form-control">
+                </div>
+            </div>
+            <br>
+
+            <!-- Autor -->
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Autor</strong>
                     <select class="form-control" type="input" name="autor_id">
                         @foreach ($autores as $autor)
                         <option value="{{ $autor->id }}">{{ $autor->nome}}</option>
@@ -68,32 +74,25 @@
                     </select>
                 </div>
             </div>
+            <br>
+
+            <!-- Data de Publicação -->
             <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                
-                <strong>Data de publicação:</strong>
-                <p>{{ date('Y-m-d') }}</p>
-                <input type="input" value="{{ date('Y-m-d') }}" name="datapub" class="form-control" hidden>
+                <div class="form-group">
+                    <strong>Data de Publicação</strong>
+                    <p>{{ date('Y-m-d') }}</p>
+                    <input type="input" value="{{ date('Y-m-d') }}" name="datapub" class="form-control" hidden>
+                </div>
             </div>
-        </div>
 
-        <hr class="my-4">
+            <hr class="my-4">
 
-        <!-- //? Cadastrar -->
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
-            <button type="submit" class="btn btn-success">Cadastrar</button>
-                    
-        </div>
-    </form>
+            <!-- Botões -->
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <a class="btn btn-primary" href="{{ route('autoresmanager.index') }}"> Voltar</a>
+                <button type="submit" class="btn btn-success">Cadastrar</button>       
+            </div>
+        </form>
     </div>
-
-</div>  
-</div>
-
-
-
-
-
-
+</div> 
 @endsection
